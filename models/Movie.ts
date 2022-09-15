e64bad0e341c6f81a596854e60441eddffa0626b
fiosanabria <87140079+fiosanabria@users.noreply.github.com>
2022-09-15 17:07:16 -0300

    2

--- models/Movie.ts
+++ models/Movie.ts
@@ -1,3 +1,6 @@
+import { Schema, model } from "mongoose";
+
+
 export type Movie = {
   title: string;
   year: number;
@@ -7,3 +10,13 @@ export type Movie = {
 export type MovieWithId = Movie & {
   _id: string;
 };
+
+export const MovieSchema = new Schema<Movie>({
+  title: { type: String, required: true, unique: true },
+  year: {type: Number, required: true, unique: false},
+  genres: {type:[String], required: true, unique: false}
+
+});
+
+export const MovieCollection = model<Movie>("movies", MovieSchema);
+
